45de4972417cac48cacb3134e93eb6839ee96bf1
Roman96932423 <ddent358@gmail.com>
2026-06-05 21:44:46 +0200

    Implemented workspace detail endpoint

--- backend/workspaces/urls.py
+++ backend/workspaces/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import workspace_list_create
+from .views import workspace_list_create, workspace_detail
 
 
 urlpatterns = [
 	path('', workspace_list_create),
+	path('<int:ws_id>/', workspace_detail)
 ]
--- backend/workspaces/views.py
+++ backend/workspaces/views.py
@@ -6,9 +6,22 @@ from rest_framework.permissions import IsAuthenticated
 from drf_spectacular.utils import extend_schema
 
 from .serializers import WorkspaceSerializer
-# from .models import Workspace
+from .models import Workspace
 
 
+@extend_schema(
+    request=WorkspaceSerializer,
+    responses=WorkspaceSerializer
+)
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def workspace_detail(request, ws_id):
+    workspace = get_object_or_404(Workspace, pk=ws_id, members=request.user)
+    serializer = WorkspaceSerializer(workspace)
+    
+    return Response(serializer.data)
+    
+    
 @extend_schema(
     request=WorkspaceSerializer,
     responses=WorkspaceSerializer
