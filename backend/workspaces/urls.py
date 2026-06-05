21807f0a391baad9e4807a573fbec6d37056199a
Roman96932423 <ddent358@gmail.com>
2026-06-05 21:34:42 +0200

    Implemented list workspaces endpoint

--- backend/workspaces/urls.py
+++ backend/workspaces/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import workspace_create
+from .views import workspace_list_create
 
 
 urlpatterns = [
-	path('', workspace_create),
+	path('', workspace_list_create),
 ]
--- backend/workspaces/views.py
+++ backend/workspaces/views.py
@@ -3,14 +3,25 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from drf_spectacular.utils import extend_schema
 
 from .serializers import WorkspaceSerializer
 # from .models import Workspace
 
 
-@api_view(['POST'])
+@extend_schema(
+    request=WorkspaceSerializer,
+    responses=WorkspaceSerializer
+)
+@api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
-def workspace_create(request):
+def workspace_list_create(request):
+    if request.method == 'GET':
+        workspaces = request.user.workspaces.all()
+        serializer = WorkspaceSerializer(workspaces, many=True)
+        
+        return Response(serializer.data)
+        
     if request.method == 'POST':
         serializer = WorkspaceSerializer(data=request.data)
         
